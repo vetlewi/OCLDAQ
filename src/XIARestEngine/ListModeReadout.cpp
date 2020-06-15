81990160f6c6e1d9f4b56d69a870f69b86ea27ea
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2020-06-15 16:01:06 +0200

    Debugging!!

--- src/XIARestEngine/ListModeReadout.cpp
+++ src/XIARestEngine/ListModeReadout.cpp
@@ -88,7 +88,11 @@ bool ListModeReadout::check_buffer(const int &readout_len, bool end_of_run)
 
     auto res = ( queue.RawSize() + entry_buffer.size() - min_buffer >= readout_len );
 
-    std::cout << res << std::endl;
+    std::cout << queue.RawSize() << std::endl;
+    std::cout << queue.RawSize() << std::endl;
+    std::cout << entry_buffer.size() << std::endl;
+    std::cout << min_buffer << std::endl;
+    std::cout << queue.RawSize() + entry_buffer.size() - min_buffer << std::endl;
 
     // Next we will check if the raw size of the buffer is large enough for buffer output.
     return res;
